f60e95800b687654c93aa022ffedfd51537cea1f
francisdoble <francis_doble@dlsu.edu.ph>
2023-12-28 16:32:59 +0800

    dummy data

--- filcrest/src/components/ListingCard.vue
+++ filcrest/src/components/ListingCard.vue
@@ -1,29 +1,32 @@
 <template>
-
-    <!-- Project Card 1 -->
     <div class="bg-white shadow-md border-2 border-gray-200 flex-col flex rounded-md">
-        <div class="mb-4">
-            <img
-                src="../assets/sample.jpeg"
-                alt="Project 1"
-                class="w-full h-80 object-cover"
-            />
+      <div class="mb-4">
+        <img
+          src="../assets/sample.jpeg"
+          :alt="listing.title"
+          class="w-full h-80 object-cover"
+        />
+      </div>
+      <div class="px-4 pb-2">
+        <h3 class="text-xl font-semibold mb-2">P {{ listing.price}}</h3>
+        <p class="text-black text-lg text-justify">{{ listing.description }}</p>
+        <div class="icon-container flex justify-center space-x-4 mt-4 text-md text-gray-800">
+          <span class="icon"><i class="fa fa-bed" aria-hidden="true"></i> {{ listing.bedrooms }} Bedrooms</span>
+          <span class="icon"><i class="fa fa-shower" aria-hidden="true"></i> {{ listing.bathrooms }} Bathrooms</span>
+          <span class="icon"><i class="fa fa-expand" aria-hidden="true"></i> {{ listing.floor_area }} sqm</span>
         </div>
-        <div class="px-4 pb-2">
-            <h3 class="text-xl font-semibold mb-2">P 10,000,000</h3>
-            <p class="text-black text-lg text-justify">Bungalow in Portofino Heights Las Pinas City</p>
-            <div class="icon-container flex justify-center space-x-4 mt-4 text-md text-gray-800">
-                <span class="icon"><i class="fa fa-bed" aria-hidden="true"></i> 3 Bedrooms</span>
-                <span class="icon"><i class="fa fa-shower" aria-hidden="true"></i> 2 Bathrooms</span>
-                <span class="icon"><i class="fa fa-expand" aria-hidden="true"></i> 150 sqm</span>
-            </div>
-        </div>
-        <button class="mx-4 my-2 py-2 bg-yellow-800 text-white text-lg rounded-lg">See more</button>
+      </div>
+      <button class="mx-4 my-2 py-2 bg-yellow-800 text-white text-lg rounded-lg">See more</button>
     </div>
-
-</template>
-<script>
-export default {
-  name: 'ListingCard',
-};
-</script>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ListingCard',
+    props: {
+      listing: Object
+    },
+    
+  };
+  </script>
+  
--- filcrest/src/components/Listings.vue
+++ filcrest/src/components/Listings.vue
@@ -1,14 +1,17 @@
 <script setup>
 import ListingCard from './ListingCard.vue';
+import listings from '../assets/listings.json'; // Assuming listings.json is placed inside the src/assets directory.
 </script>
 <template>
     <section id="projects" class="border-2 border-black py-16 min-h-[70vh]">
         <div class="container mx-auto">
             <h2 class="text-3xl md:text-4xl text-black font-semibold mb-4">Featured</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <ListingCard/>
-                <ListingCard/>
-                <ListingCard/>
+            <div class="grid grid-cols-4 gap-6">
+                <ListingCard
+                    v-for="listing in listings"
+                    :key="listing.listing_num"
+                    :listing="listing"
+                />
             </div>
         </div>
     </section>
